d49fb237694a2b4113ddc4a3e3656066bbd30316
tealium <tealium@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-19 13:07:33 +0000

    Tagging version 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/tealium/tags/1.5.1@877852 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tealium.options.php
+++ tealium.options.php
@@ -1,17 +1,93 @@
-<div class="wrap">
-	<h2>Tealium Tag Settings</h2>
+<?php
+// Create a dropdown field
+function select( $id, $options, $multiple = false ) {
+	$opt    = get_option( $id );
+	$output = '<select class="select" name="' . $id . '" id="' . $id . '">';
+	foreach ( $options as $val => $name ) {
+		$sel = '';
+		if ( $opt == $val )
+			$sel = ' selected="selected"';
+		if ( $name == '' )
+			$name = $val;
+		$output .= '<option value="' . $val . '"' . $sel . '>' . $name . '</option>';
+	}
+	$output .= '</select>';
+	return $output;
+}
 
-	<p>Paste your Tealium tag code below to add it to your site:</p>
+function generateBulkDataSourceList() {
+	global $wpdb;
+	$metaKeys = $wpdb->get_results( "SELECT DISTINCT(meta_key) FROM {$wpdb->postmeta} ORDER BY meta_id DESC" );
+	
+	$string = ', "Data Layer", "Imported from Wordpress"&#13;&#10;';
+	if ( $metaKeys ) {
+		$output = '';
+		foreach ( $metaKeys as $metaKey ) {
+			$output .= $metaKey->meta_key . $string;
+		}	
+	}
+	
+	$output .= 'siteName'. $string;
+	$output .= 'siteDescription'. $string;
+	$output .= 'pageType'. $string;
+	return $output;
+}
+
+?>
+
+<div class="wrap">
+	<h2><?php _e( 'Tealium Settings', 'tealium' ); ?></h2>
 
 	<form method="post" action="options.php">
 		<?php wp_nonce_field( 'update-options' ); ?>
 		<?php settings_fields( 'tealiumTag' ); ?>
 
-		<textarea name="tealiumTagCode" rows="10" cols="100"><?php echo get_option( 'tealiumTagCode' ); ?></textarea>
+		<p>
+			<?php _e( 'Paste your Tealium tag code below to add it to your site:', 'tealium' ); ?>
+			<br />
+			<textarea name="tealiumTagCode" rows="10" cols="100"><?php echo get_option( 'tealiumTagCode' ); ?></textarea>
+		</p>
+
+		<h3><?php _e( 'Advanced Settings', 'tealium' ); ?></h3>
+		<p>
+			<?php _e( 'Tealium tag location:', 'tealium' ); ?>
+			<br />
+			<?php
+			$options = array();
+			$options[] = __( 'After opening body tag (recommended)', 'tealium' );
+			$options[] = __( 'Header - Before closing head tag', 'tealium' );
+			$options[] = __( 'Footer - Before closing body tag', 'tealium' );
+			$options[] = __( 'Immediately after opening head tag', 'tealium' );
+			echo select( 'tealiumTagLocation', $options );
+			?>
+		</p>
+		<p>
+			<?php _e( 'Data layer style:', 'tealium' ); ?>
+			<br />
+			<?php
+			$options = array();
+			$options[] = __( 'CamelCase (legacy)', 'tealium' );
+			$options[] = __( 'Underscore (recommended)', 'tealium' );
+			echo select( 'tealiumDataStyle', $options );
+			?>
+			<br />
+			<small><?php _e( 'For example CamelCase = <i>postDate, siteName</i>. Underscore = <i>post_date, site_name</i>.', 'tealium' ); ?></small>
+		</p>
+		<p>
+			<?php _e( 'Keys to exclude from data object:', 'tealium' ); ?>
+			<br />
+			<input name='tealiumExclusions' size='50' type='text' value='<?php echo get_option( 'tealiumExclusions' ); ?>' />
+			<br />
+			<small><?php _e( 'Comma separated list - <i>postDate, custom_field_1</i>.', 'tealium' ); ?></small>
+		</p>
 
 		<input type="hidden" name="action" value="update" />
 
-		<p class="submit"><input type="submit" class="button-primary" value="<?php _e( 'Save Changes' ) ?>" /></p>
+		<p class="submit"><input type="submit" class="button-primary" value="<?php _e( 'Save Changes', 'tealium' ); ?>" /></p>
 
 	</form>
+	
+	<!-- Coming soon! -->
+	<!--<h2><?php _e( 'Data Source Bulk Export', 'tealium' ); ?></h2>
+	<textarea name="csvExport" rows="10" cols="100"><?php echo generateBulkDataSourceList() ?></textarea>-->
 </div>
--- tealium.php
+++ tealium.php
@@ -2,48 +2,157 @@
 /*
 Plugin Name: Tealium
 Plugin URI: http://tealium.com
-Description: Adds the Tealium tag and creates a data object from post data.
-Version: 1.0
-Author: Ian Hampton
-Author URI: http://ianhampton.net
+Description: Adds the Tealium tag and creates a data layer for your Wordpress site.
+Version: 1.5.1
+Author: Ian Hampton - Tealium EMEA
+Author URI: http://tealium.com
+Text Domain: tealium
 */
 
 function activate_tealium() {
+
+	// Only set data style to underscore for fresh installations
+	if ( !get_option( 'tealiumTag' ) ) {
+		update_option( 'tealiumDataStyle', '1' );
+	}
+	else {
+		add_option( 'tealiumDataStyle', '' );
+	}
+
 	add_option( 'tealiumTag', '' );
 	add_option( 'tealiumTagCode', '' );
+	add_option( 'tealiumTagLocation', '' );
+	add_option( 'tealiumExclusions', '' );
 }
 
 function deactive_tealium() {
+	delete_option( 'tealiumExclusions' );
+	delete_option( 'tealiumDataStyle', '' );
+	delete_option( 'tealiumTagLocation' );
 	delete_option( 'tealiumTagCode' );
 	delete_option( 'tealiumTag' );
 }
 
 function admin_init_tealium() {
 	register_setting( 'tealiumTag', 'tealiumTagCode' );
+	register_setting( 'tealiumTag', 'tealiumTagLocation' );
+	register_setting( 'tealiumTag', 'tealiumDataStyle' );
+	register_setting( 'tealiumTag', 'tealiumExclusions' );
 }
 
 function admin_menu_tealium() {
-	add_options_page( 'Tealium Tag Settings', 'Tealium Tag Settings', 8, 'tealium', 'options_page_tealium' );
+	add_options_page( __( 'Tealium Tag Settings', 'tealium' ), __( 'Tealium Settings', 'tealium' ), 'manage_options' , 'tealium', 'options_page_tealium' );
 }
 
 function options_page_tealium() {
 	include plugin_dir_path( __FILE__ ).'tealium.options.php';
 }
 
+/*
+ * Add an admin message when looking at the plugins page if the Tealium tag is not found
+ */
 function admin_notices_tealium() {
 	global $pagenow;
 	$tealiumTagCode = get_option( 'tealiumTagCode' );
+
 	if ( ( $pagenow == 'plugins.php' ) && ( empty( $tealiumTagCode ) ) ) {
 		$html = '<div class="updated">';
 		$html .= '<p>';
-		$html .= 'Please enter your Tealium tag code <a href="admin.php?page=tealium">over here</a>.';
+		$html .= sprintf( __( 'Please enter your Tealium tag code <a href="%s">over here</a>.', 'tealium' ), esc_url( 'options-general.php?page=tealium' ) );
 		$html .= '</p>';
 		$html .= '</div>';
 		echo $html;
 	}
 }
 
+/*
+ * Removes exclusions listed in admin setting
+ */
+function removeExclusions( $utagdata ) {
+	$exclusions = get_option( 'tealiumExclusions' );
+	if ( !empty( $exclusions ) ) {
+
+		// Convert list to array and trim whitespace
+		$exclusions = array_map( 'trim', explode( ',', $exclusions ) );
+
+		foreach ( $exclusions as $exclusion ) {
+			if ( array_key_exists( $exclusion, $utagdata ) ) {
+				// Remove from utag data array
+				unset( $utagdata[$exclusion] );
+			}
+		}
+	}
+	return $utagdata;
+}
+add_filter( 'tealium_removeExclusions', 'removeExclusions' );
+
+
+/*
+ * Convert camel case to underscores
+ */
+function convertCamelCase( $utagdata, $arrayHolder = array() ) {
+	$underscoreArray = !empty( $arrayHolder ) ? $arrayHolder : array();
+	foreach ( $utagdata as $key => $val ) {
+		$newKey = preg_replace( '/[A-Z]/', '_$0', $key );
+		$newKey = strtolower( $newKey );
+		$newKey = ltrim( $newKey, '_' );
+		if ( !is_array( $val ) ) {
+			$underscoreArray[$newKey] = $val;
+		} else {
+			$underscoreArray[$newKey] = convertCamelCase( $val, $underscoreArray[$newKey] );
+		}
+	}
+	return $underscoreArray;
+}
+add_filter( 'tealium_convertCamelCase', 'convertCamelCase' );
+
+
+/*
+ * Adds WooCommerce data to data layer
+ */
+function wooCommerceData( $utagdata ) {
+	global $woocommerce;
+
+	// Get cart details
+	$woocart = (array) $woocommerce->cart;
+	$productData = array();
+
+	if ( !empty( $woocart['cart_contents'] ) ) {
+
+		// Get cart product IDs, SKUs, Titles etc.
+		foreach ( $woocart['cart_contents'] as $cartItem ) {
+			$productMeta = new WC_Product( $cartItem['product_id'] );
+
+			$productData['product_id'][] = $cartItem['product_id'];
+			$productData['product_sku'][] = $productMeta->post->sku;
+			$productData['product_name'][] = $productMeta->post->post->post_title;
+			$productData['product_quantity'][] = $cartItem['quantity'];
+			$productData['product_regular_price'][] = get_post_meta( $cartItem['product_id'], '_regular_price', true );
+			$productData['product_sale_price'][] = get_post_meta( $cartItem['product_id'], '_sale_price', true );
+			$productData['product_type'][] = $productMeta->post->product_type;
+		}
+	}
+
+	// Remove the extensive individual product details
+	unset( $woocart['cart_contents'] );
+	unset( $woocart['tax'] );
+
+	// Get currency in use
+	$woocart['site_currency'] = get_woocommerce_currency();
+
+	// Merge shop and cart details into utagdata
+	$utagdata = array_merge( $utagdata, $woocart );
+	$utagdata = array_merge( $utagdata, $productData );
+
+	return $utagdata;
+}
+add_filter( 'tealium_wooCommerceData', 'wooCommerceData' );
+
+/*
+ * Creates the data object as an array
+ */
 function dataObject() {
+	global $utagdata;
 	$utagdata = array();
 
 	// Blog info
@@ -56,6 +165,7 @@ function dataObject() {
 		// Get categories
 		$categories = get_the_category();
 		$catout = array();
+
 		if ( $categories ) {
 			foreach ( $categories as $category ) {
 				$catout[] = $category->slug;
@@ -92,34 +202,151 @@ function dataObject() {
 			$utagdata['pageType'] = "homepage";
 		}
 	else if ( is_search() ) {
+			// Collect search and result data
+			$searchQuery = get_search_query();
+			$searchResults = &new WP_Query( 's='.str_replace( ' ', '+', $searchQuery.'&showposts=-1' ) );
+			$searchCount = $searchResults->post_count;
+			wp_reset_query();
+
+			// Add to udo
 			$utagdata['pageType'] = "search";
-			$utagdata['searchQuery'] = get_search_query();
+			$utagdata['searchQuery'] = $searchQuery;
+			$utagdata['searchResults'] = $searchCount;
 		}
 
+	// Add shop data if WooCommerce is installed
+	if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+		$utagdata = apply_filters( 'tealium_wooCommerceData', $utagdata );
+	}
+
+	// Include data layer additions from action if set
+	if ( has_action( 'tealium_addToDataObject' ) ) {
+		do_action( 'tealium_addToDataObject' );
+	}
+
+	if ( get_option( 'tealiumDataStyle' ) == '1' ) {
+		// Convert camel case to underscore
+		$utagdata = apply_filters( 'tealium_convertCamelCase', $utagdata );
+	}
+
+	// Remove excluded keys
+	$utagdata = apply_filters( 'tealium_removeExclusions', $utagdata );
+
+	return $utagdata;
+}
+
+/*
+ * Encodes the data object array as JSON, outputs script tag
+ */
+function encodedDataObject( $return = false ) {
+	$utagdata = dataObject();
+
 	// Encode data object
-	$jsondata = json_encode( $utagdata );
+	if ( version_compare( phpversion(), '5.4.0', '>=' ) ) {
+		// Pretty print JSON if PHP version supports it
+		$jsondata = json_encode( $utagdata, JSON_PRETTY_PRINT );
+	}
+	else {
+		$jsondata = json_encode( $utagdata );
+	}
 
 	// Output data object
 	if ( json_decode( $jsondata ) !== null ) {
-		echo "<script type=\"text/javascript\">
-				var utag_data = {$jsondata};
-			  </script>";
+		$utag_data = "<script type=\"text/javascript\">\nvar utag_data = {$jsondata};\n</script>\n";
+		if ( !$return ) {
+			echo $utag_data;
+		}
+		else {
+			return $utag_data;
+		}
 	}
 }
 
+/*
+ * Get the Tealium tag code, applying filters if necessary
+ */
+function getTealiumTagCode() {
+	global $tealiumtag;
+	$tealiumtag = get_option( 'tealiumTagCode' );
+
+	// Include tag action if set
+	if ( has_action( 'tealium_tagCode' ) ) {
+		do_action( 'tealium_tagCode' );
+	}
+
+	return $tealiumtag;
+}
+
+function outputTealiumTagCode() {
+	echo getTealiumTagCode();
+}
+
+/*
+ * Enable output buffer
+ */
 function outputFilter( $template ) {
 	ob_start();
 	return $template;
 }
 
-function tealiumTag() {
-	$tealiumTagCode = get_option( 'tealiumTagCode' );
-	if ( !empty( $tealiumTagCode ) ) {
-		$content = ob_get_clean();
+/*
+ * Used in combination with outputFilter() to add Tealium tag after <body>
+ */
+function tealiumTagBody( $tealiumTagCode ) {
+	$content = ob_get_clean();
+	$tealiumTagCode = getTealiumTagCode();
 
-		// Insert Tealium tag after body tag (sadly there is no wp_body hook)
-		$content = preg_replace( '#<body([^>]*)>#i', "<body$1>\n\n\t{$tealiumTagCode}", $content );
-		echo $content;
+	// Insert Tealium tag after body tag (sadly there is no wp_body hook)
+	$content = preg_replace( '#<body([^>]*)>#i', "<body$1>\n\n\t{$tealiumTagCode}", $content, 1 );
+	echo $content;
+}
+
+/*
+ * Used in combination with outputFilter() to add Tealium tag after <head>
+ */
+function tealiumTagHead( $tealiumTagCode ) {
+	$content = ob_get_clean();
+	$tealiumTagCode = getTealiumTagCode();
+	$tealiumDataObject = encodedDataObject( true );
+
+	// Insert Tealium tag immediately after head tag
+	$content = preg_replace( '#<head([^>]*)>#i', "<head$1>\n{$tealiumDataObject}\n\t{$tealiumTagCode}", $content, 1 );
+	echo $content;
+}
+
+/*
+ * Determine where the Tealium tag should be located and insert it
+ */
+function insertTealiumTag() {
+	$tealiumTagLocation = get_option( 'tealiumTagLocation' );
+	$tealiumTagCode = getTealiumTagCode();
+
+	if ( !empty( $tealiumTagCode ) ) {
+		switch ( $tealiumTagLocation ) {
+		case '1':
+			// Location - Header
+			add_action( 'wp_head', 'outputTealiumTagCode', 10000 );
+			break;
+		case '2':
+			// Location - Footer
+			add_action( 'wp_footer', 'outputTealiumTagCode', 10000 );
+			break;
+		case '3':
+			// Location - Header (Top)
+			// Start content buffer
+			add_filter( 'template_include', 'outputFilter', 1 );
+			// Inject Tealium tag, output page contents
+			add_filter( 'shutdown', 'tealiumTagHead', 0 );
+			break;
+		case '0':
+		default:
+			// Location - After opening body tag
+			// Start content buffer
+			add_filter( 'template_include', 'outputFilter', 1 );
+			// Inject Tealium tag, output page contents
+			add_filter( 'shutdown', 'tealiumTagBody', 0 );
+			break;
+		}
 	}
 }
 
@@ -131,8 +358,12 @@ if ( is_admin() ) {
 	add_action( 'admin_notices', 'admin_notices_tealium' );
 }
 
-add_action( 'wp_head', 'dataObject' );
-add_filter( 'template_include', 'outputFilter', 1 );
-add_filter( 'shutdown', 'tealiumTag', 0 );
+// Insert the Tealium tag
+add_action( 'init', 'insertTealiumTag' );
+
+// Insert the data object
+if ( get_option( 'tealiumTagLocation' ) != '3' ) {
+	add_action( 'wp_head', 'encodedDataObject', 0 );
+}
 
 ?>
